c0f25d0f84c4cd25a317079a8d622cbbf4ae3986
ash-silver <passlion@naver.com>
2023-05-25 13:39:13 +0900

    first git commit test

---
+++ src/main/java/com/example/ashsilverpage/AshsilverPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.ashsilverpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshsilverPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AshsilverPageApplication.class, args);
+    }
+
+}
