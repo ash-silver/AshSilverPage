429701a6974488231bbed6657379da2b160abe4d
ash-silver <passlion@naver.com>
2023-06-01 17:26:18 +0900

    controller route edit, controller home mapping
    staged files

--- src/main/java/com/example/ashsilverpage/controller/MemberController.java
+++ src/main/java/com/example/ashsilverpage/controller/MemberController.java
@@ -1,7 +1,17 @@
-package com.project.controller;
+package com.example.ashsilverpage.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    @GetMapping("/home")
+    public String home(){
+
+        return "home";
+    }
+
 }
