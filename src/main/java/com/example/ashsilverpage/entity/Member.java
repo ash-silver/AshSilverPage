2a05f2d95596681be16256598212f6ac5cc42211
ash-silver <passlion@naver.com>
2023-06-28 17:26:59 +0900

    member entity code

--- src/main/java/com/example/ashsilverpage/entity/Member.java
+++ src/main/java/com/example/ashsilverpage/entity/Member.java
@@ -3,10 +3,23 @@ package com.example.ashsilverpage.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
-
+@Table(name = "member")
 public class Member {
+    @Id
+    private Long Id;
 
+
+    private String m_nickname;
+    private String m_email;
+    private String m_password;
+    private String m_telephone;
+    private LocalDateTime m_joinAt = LocalDateTime.now();
+    private int m_editAt;
+
+    private int secessionAt;
 
 }
--- src/main/java/com/example/ashsilverpage/service/MemberService.java
+++ src/main/java/com/example/ashsilverpage/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.example.ashsilverpage.service;
 
+import com.example.ashsilverpage.entity.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +8,5 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberService {
 
-
+    private final MemberRepository memberRepository;
 }
